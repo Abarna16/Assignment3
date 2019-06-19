5ed2c0d361aabffa4136c5cbde3d4a9f0d2586cd
Abarna16 <47447958+Abarna16@users.noreply.github.com>
2019-06-19 20:31:04 +0530

    Add files via upload

---
+++ Split.java
@@ -0,0 +1,17 @@
+
+package Three;
+
+import java.util.Scanner;
+
+public class Split {
+     public static void main(String args[])
+    {
+        String value1,value2,value3;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter String ");
+        value1=scanner.nextLine();
+        String a[]=value1.split(" ",2);
+        for(String b:a)
+        System.out.println(b);
+    }
+}
